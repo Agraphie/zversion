70273491ec75d8c119fa21f0452f7153ed5ac558
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-04-22 13:00:28 +0200

    Extract work function, add parsing

--- http1/httpversion.go
+++ http1/httpversion.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/agraphie/zversion/worker"
+	"sync"
+	"strings"
+	"unicode"
 )
 
 type Entry struct {
@@ -13,24 +16,62 @@ type Entry struct {
 		      Write string
 	      }
 	Agent string
+	Error string
 }
 
-func startWorking(queue chan string) {
-	for line := range queue {
-		u := &Entry{}
-		json.Unmarshal([]byte(line), &u)
-		fmt.Println(u.Data.Read)
-	}
+var hosts = struct {
+	sync.RWMutex
+	m map[string][]Entry
+}{m: make(map[string][]Entry)}
+
+func (e Entry) String() string {
+	return fmt.Sprintf("IP: %v, Scanned: %v, Agent: %v", e.BaseEntry.IP, e.BaseEntry.Timestamp, e.Agent)
 }
 
 func ParseHttpFile(path string) {
-	httpEntry := func(queue chan string) {
-		for line := range queue {
-			u := &Entry{}
-			json.Unmarshal([]byte(line), &u)
-			fmt.Println(u)
+	worker.ParseFile(path, workOnLine)
+	fmt.Println(hosts.m)
+}
+
+func spaceMap(str string) string {
+	return strings.Map(func(r rune) rune {
+		if unicode.IsSpace(r) {
+			return -1
+		}
+		return r
+	}, str)
+}
+
+func workOnLine(queue chan string) {
+	for line := range queue {
+		u := Entry{}
+		json.Unmarshal([]byte(line), &u)
+		i1 := len(u.Data.Read) > 0
+		contains := strings.Contains(u.Data.Read, "Server")
+
+		switch {
+		case i1 && contains:
+			splittedString := strings.Split(u.Data.Read, "\n")
+
+			for i := range splittedString {
+				if strings.Contains(splittedString[i], "Server") {
+					serverSplit := strings.Split(splittedString[i], ":")
+					u.Agent = spaceMap(serverSplit[1])
+					hosts.Lock()
+					hosts.m[u.Agent] = append(hosts.m[u.Agent], u)
+
+					hosts.Unlock()
+				}
+			}
+		case i1 && !contains:
+			hosts.Lock()
+			u.Agent = "Not set"
+			hosts.m["No Server"] = append(hosts.m["No Server"], u)
+			hosts.Unlock()
+		default:
+			hosts.Lock()
+			hosts.m["Error"] = append(hosts.m["Error"], u)
+			hosts.Unlock()
 		}
 	}
-
-	worker.ParseFile(path, httpEntry)
 }
