c8714d91a36714f545443b1cf9b39f84b391325d
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-04-22 20:25:33 +0200

    Add constants, add writing of result to file

--- http1/httpversion.go
+++ http1/httpversion.go
@@ -7,14 +7,27 @@ import (
 	"sync"
 	"strings"
 	"unicode"
+	"bufio"
+	"os"
+	"time"
 )
+
+const NO_AGENT = "Not set"
+const NO_AGENT_KEY = "Not set"
+const ERROR_KEY = "Error"
+const SERVER_AGENT_STRING = "Server"
+const SERVER_AGENT_DELIMITER = ":"
+const OUTPUT_FILE_LOCATION = "http_version"
+const OUTPUT_FILE_ENDING = ".json"
+const TIMESTAMP_FORMAT = "2006-01-02-15:04"
+
 
 type Entry struct {
 	worker.BaseEntry
 	Data  struct {
-		      Read  string
-		      Write string
-	      }
+			  Read  string
+			  Write string
+		  }
 	Agent string
 	Error string
 }
@@ -28,12 +41,14 @@ func (e Entry) String() string {
 	return fmt.Sprintf("IP: %v, Scanned: %v, Agent: %v", e.BaseEntry.IP, e.BaseEntry.Timestamp, e.Agent)
 }
 
-func ParseHttpFile(path string) {
+func ParseHttpFile(path string) map[string][]Entry {
 	worker.ParseFile(path, workOnLine)
-	fmt.Println(hosts.m)
+
+	writeMapToFile(OUTPUT_FILE_LOCATION, hosts.m)
+	return hosts.m
 }
 
-func spaceMap(str string) string {
+func removeSpaces(str string) string {
 	return strings.Map(func(r rune) rune {
 		if unicode.IsSpace(r) {
 			return -1
@@ -46,32 +61,62 @@ func workOnLine(queue chan string) {
 	for line := range queue {
 		u := Entry{}
 		json.Unmarshal([]byte(line), &u)
-		i1 := len(u.Data.Read) > 0
-		contains := strings.Contains(u.Data.Read, "Server")
+		dataAvailable := len(u.Data.Read) > 0
+		contains := strings.Contains(u.Data.Read, SERVER_AGENT_STRING)
+
+		if dataAvailable {
+			u.Data.Read = strings.Replace(u.Data.Read, "\r\n", "\n", -1)
+			u.Data.Read = strings.Replace(u.Data.Read, "\n\n", "\n", -1)
+		}
+
+		var key string
 
 		switch {
-		case i1 && contains:
+		case dataAvailable && contains:
 			splittedString := strings.Split(u.Data.Read, "\n")
-
 			for i := range splittedString {
-				if strings.Contains(splittedString[i], "Server") {
-					serverSplit := strings.Split(splittedString[i], ":")
-					u.Agent = spaceMap(serverSplit[1])
-					hosts.Lock()
-					hosts.m[u.Agent] = append(hosts.m[u.Agent], u)
+				if strings.Contains(splittedString[i], SERVER_AGENT_STRING) {
+					serverSplit := strings.Split(splittedString[i], SERVER_AGENT_DELIMITER)
+					u.Agent = removeSpaces(serverSplit[1])
+					key = u.Agent
 
-					hosts.Unlock()
+					break
 				}
 			}
-		case i1 && !contains:
-			hosts.Lock()
-			u.Agent = "Not set"
-			hosts.m["No Server"] = append(hosts.m["No Server"], u)
-			hosts.Unlock()
+		case dataAvailable && !contains:
+			u.Agent = NO_AGENT
+			key = NO_AGENT_KEY
 		default:
-			hosts.Lock()
-			hosts.m["Error"] = append(hosts.m["Error"], u)
-			hosts.Unlock()
+			key = ERROR_KEY
 		}
+		addToMap(key, u)
+	}
+}
+
+func addToMap(key string, entry Entry) {
+	hosts.Lock()
+	hosts.m[key] = append(hosts.m[key], entry)
+	hosts.Unlock()
+}
+
+func writeMapToFile(path string, hosts map[string][]Entry) {
+	timestamp := time.Now().Format(TIMESTAMP_FORMAT)
+	f, err := os.Create(path + "_" + timestamp + OUTPUT_FILE_ENDING)
+	check(err)
+	defer f.Close()
+
+	j, jerr := json.MarshalIndent(hosts, "", "  ")
+	if jerr != nil {
+		fmt.Println("jerr:", jerr.Error())
+	}
+
+	w := bufio.NewWriter(f)
+	w.Write(j)
+	w.Flush()
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
 	}
 }
