f7367e2f41a1dc78ca791fe3675c65611551ed54
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-04-25 13:59:00 +0200

    Add simple command starter

--- zversion.go
+++ zversion.go
@@ -1,13 +1,93 @@
 package main
 
 import (
-	"github.com/agraphie/zversion/http1"
+	"flag"
+	"os/exec"
+	"log"
+	"strings"
+	"fmt"
+	"bufio"
+	"os"
+	"os/signal"
+	"syscall"
+	"errors"
 )
 
+
+var (
+ strFlag = flag.String("long-string", "", "Description")
+ portFlag = flag.String("port", "80", "The port to scan")
+ scanTargets = flag.String("targets", "100%", "How many targets should be scanned, absolute or percentage value")
+ scanOutputPath = flag.String("scan-output", "scanResults/", "File path to output scan result")
+ analysisOutputPath = flag.String("analysis-output", "analysisResults/", "File path to output analysis results")
+)
+
+
+func init(){
+	flag.StringVar(strFlag, "s", "", "Description")
+	flag.StringVar(portFlag, "p", "80", "The port to scan")
+	flag.StringVar(scanTargets, "n", "100%", "The port to scan")
+	flag.StringVar(scanOutputPath, "so", "scanResults/", "File path to output scan results")
+	flag.StringVar(analysisOutputPath, "ao", "analysisResults/", "File path to output analaysis results")
+
+
+	flag.Parse()
+
+}
 func main() {
-	http1.ParseHttpFile("/home/agraphie/banners.json")
+	execCommandWithCancel("sleep 5")
 }
 
+func LaunchHttpScan(){
+
+}
+
+func execCommandWithCancel(command string){
+	execCommand := strings.Fields(command)
+	cmd := exec.Command(execCommand[0], execCommand[1:]...)
+	stdout, _ := cmd.StdoutPipe()
+
+
+	done := make(chan error, 1)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	cmd.Start()
+
+	go func(){
+		sig := <-sigs
+		switch sig {
+		case os.Interrupt:
+		done <- errors.New("Interrupted")
+		case syscall.SIGTERM:
+		done <- errors.New("Terminated")
+		}
+	}()
+
+	go func() {
+		done <- cmd.Wait()
+	}()
+
+
+
+
+
+	in := bufio.NewScanner(stdout)
+
+	for in.Scan() {
+		fmt.Println("meh")
+	}
+
+	select {
+	case err := <-done:
+		if err != nil {
+			//cmd.Process.Kill()
+			log.Printf("process done with error = %v", err)
+		} else {
+			log.Print("process done gracefully without error")
+		}
+	}
+}
 func Meh() {
-
+	//sudo zmap -p 80 -n 1000 -q | ztee results.csv | ./zgrab --port 80 --data=./http-req-head --output-file=/home/agraphie/banners5.json
 }
+
