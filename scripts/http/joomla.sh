fe68e1d3c05abac059d68a994dea95dd0c1f0237
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-07-10 19:46:56 +0200

    Fix output filename

--- scripts/http/joomla.sh
+++ scripts/http/joomla.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#Ouput filename: joomla_version_distribution
+#Output filename: joomla_version_distribution
 printf $1
 printf "`jq '.CMS[] | select(.Vendor == "Joomla") | .Version' $1 | sort | uniq -c | sort -nr`"
--- scripts/http/wordpress.sh
+++ scripts/http/wordpress.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
+#Output filename: wordpress_version_distribution
 printf $1
-printf "\n WordPress \n"
 printf "`jq '.CMS[] | select(.Vendor == "WordPress") | .Version' $1 | sort | uniq -c | sort -nr`"
-printf "\n done \n"
