d95ceac909846283fdf2da5904dcf8a3fcda78de
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-07-10 19:43:04 +0200

    Change output file names

--- scripts/http/apacheLt.sh
+++ scripts/http/apacheLt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#Output filename: apache_cve-2015-3183
 printf $1
 printf "\n"
-printf "Version < 2.4.14: `jq '.Agents[] | select(.Agent=="Apache" and .CanonicalVersion < "0002000400140000" and .Version != "") | .Version' $1 | wc -l`"
+printf "CVE-2015-3183 (Version < 2.4.14): `jq '.Agents[] | select(.Agent=="Apache" and .CanonicalVersion < "0002000400140000" and .Version != "") | .Version' $1 | wc -l`"
 printf "\n"
 printf "Total: `jq '.Agents[] | select(.Agent=="Apache") | .Agent' $1 | wc -l`"
 printf "\n"
--- scripts/ssh/openSSHLt.sh
+++ scripts/ssh/openSSHLt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Output file name: OpenSSH_CVE_2016-0777_and_2016-0778
+#Output file name: openSSH_CVE_2016-0777_and_2016-0778
 printf $1
 printf "\n"
 printf "5.4 >= OpenSSH Version <= 7.1: `jq 'select(.Vendor =="OpenSSH" and .CanonicalVersion <= "0007000100000000" and .CanonicalVersion >= "0005000400000000" and .SoftwareVersion != "" and .SoftwareVersion != "7.1p1") | .SoftwareVersion' $1 | wc -l`"
