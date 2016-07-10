f6570695b040f998b671c4de4125c6222120c929
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-07-10 19:39:06 +0200

    Add output filename, change output format

--- scripts/http/drupal.sh
+++ scripts/http/drupal.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
+#Output filename: drupal_version_distribution
 printf $1
-printf "\n Drupal \n"
 printf "`jq '.CMS[] | select(.Vendor == "Drupal") | .Version' $1 | sort | uniq -c | sort -nr`"
-printf "\n done \n"
