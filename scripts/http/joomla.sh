ca570a634dca71c144691c5fc3d7af66374f371f
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-07-10 19:38:31 +0200

    Add filename, change output format

--- scripts/http/joomla.sh
+++ scripts/http/joomla.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
+#Ouput filename: joomla_version_distribution
 printf $1
-printf "\n Joomla \n"
 printf "`jq '.CMS[] | select(.Vendor == "Joomla") | .Version' $1 | sort | uniq -c | sort -nr`"
-printf "\n Done \n"
