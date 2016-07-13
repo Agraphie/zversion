15161ca41fda097a4a2648161541106ebf76c727
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-07-13 12:03:56 +0200

    Add simple script to print versions upgraded to wordpress 5.4.3

---
+++ scripts/http/wordpressUpdateBehaviour.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"github.com/agraphie/zversion/http1"
+	"log"
+	"os"
+)
+
+func main() {
+	entries := map[string]string{}
+	sum := map[string]int{}
+
+	filepathNew := os.Args[1]
+	file, err := os.Open(filepathNew)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var entry http1.ZversionEntry
+	for scanner.Scan() {
+		line := scanner.Text()
+		json.Unmarshal([]byte(line), &entry)
+
+		if len(entry.CMS) > 0 {
+			for _, v := range entry.CMS {
+				if v.Vendor == "WordPress" && v.CanonicalVersion >= "0004000500030000" {
+					entries[entry.IP] = v.Version
+				}
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	filepathOld := os.Args[2]
+	file1, err1 := os.Open(filepathOld)
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+	defer file.Close()
+
+	scanner1 := bufio.NewScanner(file1)
+	for scanner1.Scan() {
+		line := scanner1.Text()
+		json.Unmarshal([]byte(line), &entry)
+
+		if _, ok := entries[entry.IP]; ok {
+			if len(entry.CMS) > 0 {
+				for _, v := range entry.CMS {
+					if v.Vendor == "WordPress" {
+						sum[v.Version]++
+					}
+				}
+			}
+		}
+	}
+
+	if err1 := scanner1.Err(); err1 != nil {
+		log.Fatal(err1)
+	}
+	log.Println(sum)
+}
