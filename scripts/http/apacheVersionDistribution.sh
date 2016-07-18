c37a49551ed81fa06261d4d6571e6ddb6097b994
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-07-18 21:16:04 +0200

    Add script for server distribution of continent and countries

--- scripts/http/apacheVersionDistribution.sh
+++ scripts/http/apacheVersionDistribution.sh
@@ -4,4 +4,5 @@ printf "Script name: $0\n"
 printf "Input file: $1\n"
 printf '%s\n' '-------------Apache version distribution-------------'
 printf "`grep "Apache" $1 | jq '.Agents[] | select(.Vendor == "Apache") | .Version' | sort | uniq -c | sort -nr` \n"
+printf  '\nTotal: %s\n' "`grep "Apache" $1 | jq '.Agents[] | select(.Vendor == "Apache") | .Version' | wc -l`"
 printf '%s\n' '-----------------------------------------------------'
--- scripts/http/nginxVersionDistribution.sh
+++ scripts/http/nginxVersionDistribution.sh
@@ -4,4 +4,5 @@ printf "Script name: $0\n"
 printf "Input file: $1\n"
 printf '%s\n' '-------------Nginx version distribution-------------'
 printf "`grep "nginx" $1 | jq '.Agents[] | select(.Vendor == "nginx") | .Version' | sort | uniq -c | sort -nr` \n"
+printf  '\nTotal: %s\n' "`grep "nginx" $1 | jq '.Agents[] | select(.Vendor == "nginx") | .Version' | wc -l`"
 printf '%s\n' '-----------------------------------------------------'
