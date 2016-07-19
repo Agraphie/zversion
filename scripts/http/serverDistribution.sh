e79c6d6a6ba67def7da2f74382da8b0ee3eada81
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-07-19 11:59:03 +0200

    Add script for calculating the market share of the main vendors

---
+++ scripts/http/serverDistribution.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#Output filename: major_server_vendor_distribution_for_asn
+printf "Script name: $0\n"
+printf "Input file: $1\n"
+printf '%s\n' '----------------------------------------------'
+majorServerVendors=(
+    "Microsoft-IIS"
+	"Apache"
+	"nginx"
+	"lighttpd"
+	"ATS"
+	"BOA"
+	"Allegro Software RomPager"
+	"AllegroServe"
+	"squid"
+	"tengine"
+	"jetty"
+	"RomPager"
+	"mini_httpd"
+	"micro_httpd"
+	"AOL Server"
+	"Abyss"
+	"Agranat-EmWeb"
+	"Microsoft-HTTPAPI"
+	"CherryPy"
+	"Cherokee"
+	"CommuniGatePro"
+	"EdgePrism"
+	"Flywheel"
+	"GoAhead"
+	"IdeaWebServer"
+	"Indy"
+	"mbedthis"
+	"PRTG"
+	"Kangle"
+	"thttpd")
+
+total=`wc -l < $1`
+
+printf '%s\n' "------------Total entries: $total-------------"
+
+for i in "${majorServerVendors[@]}"
+do
+    vendorCount=0
+    vendorCount=`grep "$i" $1 | jq --arg vendor "$i" '.Agents[] | select(.Vendor == $vendor) | .Vendor' |  wc -l`
+
+    printf '%s\n' "$i: $vendorCount"
+done
+printf '%s\n' '----------------------------------------------'
