3d33ece2def8045e527bb50b97852ed539ddc046
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-07-19 16:06:59 +0200

    Add more information output and fix bug

--- scripts/http/apacheLt.sh
+++ scripts/http/apacheLt.sh
@@ -3,7 +3,7 @@
 printf "Script name: $0\n"
 printf "Input file: $1\n"
 printf '%s\n' '-----------------------'
-printf "CVE-2015-3183 (Version < 2.4.14): `grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion < "0002000400140000" and .Version != "") | .Version' | wc -l`"
+printf "CVE-2015-3183 (2.4.0 <= Version < 2.4.14): `grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion < "0002000400140000" and .CanonicalVersion >= "0002000400000000" and .Version != "") | .Version' | wc -l`"
 printf "\n"
 printf "Total: `grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache") | .Vendor' | wc -l` \n"
 printf '%s\n' '-----------------------'
