3ee626140c4cfc2b4813bb54916fbaf600c455db
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-07-20 18:20:14 +0200

    Add more output

--- scripts/http/apacheVersionDistribution.sh
+++ scripts/http/apacheVersionDistribution.sh
@@ -4,5 +4,9 @@ printf "Script name: $0\n"
 printf "Input file: $1\n"
 printf '%s\n' '-------------Apache version distribution-------------'
 printf "`grep "Apache" $1 | jq '.Agents[] | select(.Vendor == "Apache") | .Version' | sort | uniq -c | sort -nr` \n"
+total24=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion >= "0002000400000000" and .Version != "") | .Vendor' | wc -l`
+total22=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion <= "0002000400000000" and .CanonicalVersion >= "0002000200000000" and .Version != "") | .Vendor' | wc -l`
+printf '\nTotal Version 2.2 <= Version <= 2.4: %s' "$total22"
+printf '\nTotal Version >= 2.4: %s' "$total24"
 printf  '\nTotal: %s\n' "`grep "Apache" $1 | jq '.Agents[] | select(.Vendor == "Apache") | .Version' | wc -l`"
 printf '%s\n' '-----------------------------------------------------'
