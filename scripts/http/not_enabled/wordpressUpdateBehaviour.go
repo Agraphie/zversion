3b46f081a65c2d627cd1329491a439df2b2aa4c0
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-07-25 22:03:15 +0200

    Add flags and capability to output server vendors OR cms vendors

--- scripts/http/not_enabled/wordpressUpdateBehaviour.go
+++ scripts/http/not_enabled/wordpressUpdateBehaviour.go
@@ -3,23 +3,43 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/agraphie/zversion/http1"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
+var (
+	oldFile          = flag.String("old-file", "", "The old file to compare")
+	newFile          = flag.String("new-file", "", "The new file to compare")
+	serverVendor     = flag.String("server-vendor", "", "The server vendor to look for")
+	cmsVendor        = flag.String("cms-vendor", "", "The CMS vendor to look for")
+	canonicalVersion = flag.String("version", "", "The version in canonical form")
+)
+
+func init() {
+	flag.StringVar(oldFile, "of", "", "The old file to compare")
+	flag.StringVar(newFile, "nf", "", "The new file to compare")
+	flag.StringVar(serverVendor, "sv", "", "The server vendor to look for")
+	flag.StringVar(cmsVendor, "cv", "", "The CMS vendor to look for")
+	flag.StringVar(canonicalVersion, "v", "", "The version in canonical form")
+
+	flag.Parse()
+
+}
+
 func main() {
+	fmt.Println(*serverVendor + *cmsVendor + " upgraded from versions in " + *oldFile + " to version " + *canonicalVersion + " in " + *newFile)
+
 	entries := map[string]string{}
 	sum := map[string]int{}
 	asn := map[string]int{}
+	updateCount := 0
 
-	filepathNew := os.Args[1]
-	filepathOld := os.Args[2]
-	vendor := os.Args[3]
-	canonicalVersion := os.Args[4]
-	file, err := os.Open(filepathNew)
+	file, err := os.Open(*newFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -33,10 +53,23 @@ func main() {
 		line := scanner.Text()
 		json.Unmarshal([]byte(line), &entry)
 
-		if len(entry.CMS) > 0 {
-			for _, v := range entry.CMS {
-				if v.Vendor == vendor && v.CanonicalVersion >= canonicalVersion {
-					entries[entry.IP] = v.Version
+		if *cmsVendor != "" {
+			if len(entry.CMS) > 0 {
+				for _, v := range entry.CMS {
+
+					if v.Vendor == *cmsVendor && v.CanonicalVersion == *canonicalVersion {
+						updateCount++
+						entries[entry.IP] = v.Version
+					}
+				}
+			}
+		} else {
+			if len(entry.Agents) > 0 {
+				for _, v := range entry.Agents {
+					if v.Vendor == *serverVendor && v.CanonicalVersion == *canonicalVersion {
+						updateCount++
+						entries[entry.IP] = v.Version
+					}
 				}
 			}
 		}
@@ -46,7 +79,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	file1, err1 := os.Open(filepathOld)
+	file1, err1 := os.Open(*oldFile)
 	if err1 != nil {
 		log.Fatal(err1)
 	}
@@ -60,11 +93,22 @@ func main() {
 		json.Unmarshal([]byte(line), &entry)
 
 		if _, ok := entries[entry.IP]; ok {
-			if len(entry.CMS) > 0 {
-				for _, v := range entry.CMS {
-					if v.Vendor == vendor {
-						sum[v.Version]++
-						asn[entry.ASId+"("+entry.ASOwner+")"]++
+			if *cmsVendor != "" {
+				if len(entry.CMS) > 0 {
+					for _, v := range entry.CMS {
+						if v.Vendor == *cmsVendor {
+							sum[v.Version]++
+							asn[entry.ASId+"("+entry.ASOwner+")"]++
+						}
+					}
+				}
+			} else {
+				if len(entry.Agents) > 0 {
+					for _, v := range entry.Agents {
+						if v.Vendor == *serverVendor {
+							sum[v.Version]++
+							asn[entry.ASId+"("+entry.ASOwner+")"]++
+						}
 					}
 				}
 			}
@@ -74,16 +118,41 @@ func main() {
 	if err1 := scanner1.Err(); err1 != nil {
 		log.Fatal(err1)
 	}
-	log.Println(vendor + " upgraded from versions in " + filepathOld + " to version " + canonicalVersion + " in " + filepathNew)
-	log.Println("-----------Upgraded from--------------")
-	for v, k := range sum {
-		fmt.Println(strconv.Itoa(k) + " " + v)
-	}
-	log.Println("--------------------------------------")
+	versionSort := rankByWordCount(sum)
+	asnSort := rankByWordCount(asn)
 
-	log.Println("-----------ASN distribution-----------")
-	for v, k := range asn {
-		fmt.Println(strconv.Itoa(k) + " " + v)
+	fmt.Printf("-----------Upgraded from (%v in total)--------------\n", updateCount)
+	for _, k := range versionSort {
+		fmt.Println(strconv.Itoa(k.Value) + " " + k.Key)
 	}
-	log.Println("--------------------------------------")
+	fmt.Println("--------------------------------------")
+	fmt.Println("")
+
+	fmt.Println("-----------ASN distribution-----------")
+	for _, k := range asnSort {
+		fmt.Println(strconv.Itoa(k.Value) + " " + k.Key)
+	}
+	fmt.Println("--------------------------------------")
 }
+
+func rankByWordCount(wordFrequencies map[string]int) PairList {
+	pl := make(PairList, len(wordFrequencies))
+	i := 0
+	for k, v := range wordFrequencies {
+		pl[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(sort.Reverse(pl))
+	return pl
+}
+
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
