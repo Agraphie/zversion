80b9362a4fa28f4724863cc065f93604d22208b2
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-08-13 20:00:27 +0200

    Fix bug

--- scripts/http/apacheVersionDistribution.sh
+++ scripts/http/apacheVersionDistribution.sh
@@ -4,10 +4,10 @@ printf "Script name: $0\n"
 printf "Input file: $1\n"
 printf '%s\n' '-------------Apache version distribution-------------'
 printf "`grep "Apache" $1 | jq '.Agents[] | select(.Vendor == "Apache") | .Version' | sort | uniq -c | sort -nr` \n"
-total24=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion >= "0002000400000000" and .Version != "") | .Vendor' | wc -l`
-total22=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion < "0002000400000000" and .CanonicalVersion >= "0002000200000000" and .Version != "") | .Vendor' | wc -l`
-total20=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion < "0002000200000000" and .CanonicalVersion >= "0002000000000000" and .Version != "") | .Vendor' | wc -l`
-total13=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion < "0002000000000000" and .CanonicalVersion >= "0001000300000000" and .Version != "") | .Vendor' | wc -l`
+total24=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion >= "0002000400000000" and .CanonicalVersion <= "0002000400230000" and  .Version != "") | .Vendor' | wc -l`
+total22=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion < "0002000400000000" and .CanonicalVersion <= "0002000200310000" and .CanonicalVersion >= "0002000200000000" and .Version != "") | .Vendor' | wc -l`
+total20=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion < "0002000200000000" and .CanonicalVersion <= "0002000000650000" and .CanonicalVersion >= "0002000000000000" and .Version != "") | .Vendor' | wc -l`
+total13=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion < "0002000000000000" and .CanonicalVersion <= "0001000300420000" and .CanonicalVersion >= "0001000300000000" and .Version != "") | .Vendor' | wc -l`
 totalVersion=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .Version != "") | .Vendor' | wc -l`
 total=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor == "Apache") | .Version' | wc -l`
 totalUnknownVersion=$((total-totalVersion))
