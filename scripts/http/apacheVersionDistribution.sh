85167280cd0648494d51a60c33acad261b396f98
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-08-13 19:03:08 +0200

    Add more versions and add percentages

--- scripts/http/apacheVersionDistribution.sh
+++ scripts/http/apacheVersionDistribution.sh
@@ -8,10 +8,17 @@ total24=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .Canoni
 total22=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion < "0002000400000000" and .CanonicalVersion >= "0002000200000000" and .Version != "") | .Vendor' | wc -l`
 total20=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion < "0002000200000000" and .CanonicalVersion >= "0002000000000000" and .Version != "") | .Vendor' | wc -l`
 total13=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion < "0002000000000000" and .CanonicalVersion >= "0001000300000000" and .Version != "") | .Vendor' | wc -l`
-
-printf '\nTotal Version 1.3 <= Version < 2.0: %s' "$total13"
+totalVersion=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .Version != "") | .Vendor' | wc -l`
+total=`grep "Apache" $1 | jq '.Agents[] | select(.Vendor == "Apache") | .Version' | wc -l`
+totalUnknownVersion=$((total-totalVersion))
+printf '\nTotal Version 1.3 <= Version < 2.0: %s' "$total13 "
 printf '\nTotal Version 2.0 <= Version < 2.2: %s' "$total20"
 printf '\nTotal Version 2.2 <= Version < 2.4: %s' "$total22"
 printf '\nTotal Version >= 2.4: %s' "$total24"
-printf  '\nTotal: %s\n' "`grep "Apache" $1 | jq '.Agents[] | select(.Vendor == "Apache") | .Version' | wc -l`"
+
+printf '\n%s' '-----------------------'
+printf '\nTotal known version: %s' "$totalVersion ($(awk "BEGIN {printf \"%.2f\n\", 100/$total*$totalVersion}")%)"
+printf '\nTotal unknown version: %s' "$totalUnknownVersion ($(awk "BEGIN {printf \"%.2f\n\", 100/$total*$totalUnknownVersion}")%)"
+
+printf  '\nTotal: %s\n' "$total"
 printf '%s\n' '-----------------------------------------------------'
