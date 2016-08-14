ca79b6eef45737d6ec657ac271d59a20fd567534
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-08-14 23:27:05 +0200

    Add more output to server distribution

--- scripts/http/serverDistribution.sh
+++ scripts/http/serverDistribution.sh
@@ -42,14 +42,16 @@ majorServerVendors=(
 	"Varnish")
 
 total=`wc -l < $1`
+totalNoErrors=`jq 'select(.Error == "") | .IP' $1 | wc -l`
 
-printf '%s\n' "------------Total entries: $total-------------"
+
+printf '%s\n' "------------Total entries: $total ($totalNoErrors no error)-------------"
 
 for i in "${majorServerVendors[@]}"
 do
     vendorCount=0
     vendorCount=`grep "$i" $1 | jq --arg vendor "$i" '.Agents[] | select(.Vendor == $vendor) | .Vendor' |  wc -l`
 
-    printf '%s\n' "$i: $vendorCount ($(awk "BEGIN {printf \"%.2f\n\", 100/$total*$vendorCount}")%)"
+    printf '%s\n' "$i: $vendorCount ($(awk "BEGIN {printf \"%.2f\n\", 100/$totalNoErrors*$vendorCount}")% of no errors)"
 done
 printf '%s\n' '----------------------------------------------'
