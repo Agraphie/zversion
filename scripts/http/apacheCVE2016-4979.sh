21dc6b3d736e2a332b93dac3d2c4136c5354584b
Clemens Keppler <clemens.keppler@rwth-aachen.de>
2016-08-16 17:50:23 +0200

    Fix condition

--- scripts/http/apacheCVE2016-4979.sh
+++ scripts/http/apacheCVE2016-4979.sh
@@ -3,8 +3,8 @@
 printf "Script name: $0\n"
 printf "Input file: $1\n"
 printf '%s\n' '-----------------------'
-printf "CVE-2016-4979 (Version 2.4.18 or 2.4.20): `grep "Apache\",\"Version\":\"2.2.18|Apache\",\"Version\":\"2.2.20" $1 | jq '.Agents[] | select(.Vendor=="Apache" and (.CanonicalVersion = "0002000400180000" or .CanonicalVersion = "0002000400200000") and .Version != "") | .Version' | wc -l`"
+printf "CVE-2016-4979 (Version 2.4.18 or 2.4.20): `grep 'Apache","Version":"2.4.18\|Apache","Version":"2.4.20' $1 | jq '.Agents[] | select(.Vendor=="Apache" and (.CanonicalVersion == "0002000400180000" or .CanonicalVersion == "0002000400200000") and .Version != "") | .Version' | wc -l`"
 printf "\n"
-printf "Total Version >= 2.4.0: `grep "Apache\",\"Version\":\"2.4" $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion >= "0002000400000000" and .Version != "") | .Vendor' | wc -l` \n"
+printf "Total Version >= 2.4.0: `grep 'Apache","Version":"2.4' $1 | jq '.Agents[] | select(.Vendor=="Apache" and .CanonicalVersion >= "0002000400000000" and .CanonicalVersion <= "0002000400230000" and  .Version != "") | .Vendor' | wc -l` \n"
 printf "Total: `grep "Apache" $1 | jq '.Agents[] | select(.Vendor=="Apache") | .Vendor' | wc -l` \n"
 printf '%s\n' '-----------------------'
